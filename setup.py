6b94b728655c648f728d6dbe586fd325b4d449a2
Shiv Haris <sharis@brocade.com>
2016-10-05 13:49:33 -0700

    Changing version to 0.2.1

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ reqs = [str(ir.req) for ir in install_reqs]
 
 setup(
     name="napalm-brcd",
-    version="0.1.0",
+    version="0.2.1",
     packages=find_packages(),
     author="Shiv Haris",
     author_email="sharis@brocade.com",
